a95c6bbcf94473c9e14f4f3500c027ade6d1b231
Matthias Frei <matzefrei@gmail.com>
2022-01-07 13:41:18 +0100

    Add authentication filter and security constants

---
+++ starter_code/src/main/java/com/example/demo/security/SecurityConfig.java
@@ -0,0 +1,11 @@
+package com.example.demo.security;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+
+}
---
+++ starter_code/src/main/java/com/example/demo/security/SecurityConstants.java
@@ -0,0 +1,10 @@
+package com.example.demo.security;
+
+public class SecurityConstants {
+
+    public static final int EXPIRATION_TIME = 60*60*24*10;
+    public static final String SECRET = "secret_key";
+    public static final String HEADER_STRING = "Authorization";
+    public static final String TOKEN_PREFIX = "Bearer";
+
+}
