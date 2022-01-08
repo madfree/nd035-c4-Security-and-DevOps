ef569dc22345ccd1d58c3664030e7ad3c7bcf3a4
Matthias Frei <matzefrei@gmail.com>
2022-01-08 14:55:27 +0100

    Add dependency and TestUtils

---
+++ starter_code/src/test/java/com/example/demo/TestUtils.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import java.lang.reflect.Field;
+
+public class TestUtils {
+
+    public static void injectObjects(Object target, String fieldName, Object toInject) {
+        boolean isPrivate = false;
+
+        try {
+            Field field = target.getClass().getDeclaredField(fieldName);
+            if (field.canAccess(target)) {
+                field.setAccessible(true);
+                isPrivate = true;
+            }
+            field.set(target, toInject);
+
+            if (isPrivate) {
+                field.setAccessible(false);
+            }
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
