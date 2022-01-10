c3f169593c1125b797c713f0cf47d1bf3be73b29
Matthias Frei <matzefrei@gmail.com>
2022-01-10 14:10:27 +0100

    Add logging for UserController and OrderController

--- starter_code/src/main/java/com/example/demo/controllers/OrderController.java
+++ starter_code/src/main/java/com/example/demo/controllers/OrderController.java
@@ -4,6 +4,8 @@ import com.example.demo.model.persistence.User;
 import com.example.demo.model.persistence.UserOrder;
 import com.example.demo.model.persistence.repositories.OrderRepository;
 import com.example.demo.model.persistence.repositories.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,7 +15,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/order")
 public class OrderController {
-	
+
+	public static final Logger log = LoggerFactory.getLogger(OrderController.class);
 	
 	@Autowired
 	private UserRepository userRepository;
@@ -26,19 +29,33 @@ public class OrderController {
 	public ResponseEntity<UserOrder> submit(@PathVariable String username) {
 		User user = userRepository.findByUsername(username);
 		if(user == null) {
+			log.error("Could not submit order. User not found: " + username);
 			return ResponseEntity.notFound().build();
 		}
 		UserOrder order = UserOrder.createFromCart(user.getCart());
 		orderRepository.save(order);
-		return ResponseEntity.ok(order);
+		ResponseEntity<UserOrder> orderResponse = ResponseEntity.ok(order);
+		if (orderResponse.getStatusCode().isError()) {
+			log.error("Error! Could not process order: " + orderResponse.getStatusCode().getReasonPhrase());
+		} else {
+			log.debug("Order successfully submitted with id: " + orderResponse.getBody().getId());
+		}
+		return orderResponse;
 	}
 	
 	@GetMapping("/history/{username}")
 	public ResponseEntity<List<UserOrder>> getOrdersForUser(@PathVariable String username) {
 		User user = userRepository.findByUsername(username);
 		if(user == null) {
+			log.error("User not found:" + username);
 			return ResponseEntity.notFound().build();
 		}
-		return ResponseEntity.ok(orderRepository.findByUser(user));
+		ResponseEntity<List<UserOrder>> userOrderHistory = ResponseEntity.ok(orderRepository.findByUser(user));
+		if (userOrderHistory.getStatusCode().isError()) {
+			log.error("Could not load user order history");
+		} else {
+			log.debug("Successfully fetched user order history");
+		}
+		return userOrderHistory;
 	}
 }
--- starter_code/src/main/java/com/example/demo/controllers/UserController.java
+++ starter_code/src/main/java/com/example/demo/controllers/UserController.java
@@ -5,6 +5,8 @@ import com.example.demo.model.persistence.User;
 import com.example.demo.model.persistence.repositories.CartRepository;
 import com.example.demo.model.persistence.repositories.UserRepository;
 import com.example.demo.model.requests.CreateUserRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -13,6 +15,8 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
+
+	public static final Logger log = LoggerFactory.getLogger(UserController.class);
 	
 	@Autowired
 	private UserRepository userRepository;
@@ -25,12 +29,24 @@ public class UserController {
 
 	@GetMapping("/id/{id}")
 	public ResponseEntity<User> findById(@PathVariable Long id) {
-		return ResponseEntity.of(userRepository.findById(id));
+		ResponseEntity<User> userResponse = ResponseEntity.of(userRepository.findById(id));
+		if (userResponse.getStatusCode().isError()) {
+			log.error("User for id not found: " + id);
+		} else {
+			log.debug("Found user with name: " + userResponse.getBody().getUsername());
+		}
+		return userResponse;
 	}
 	
 	@GetMapping("/{username}")
 	public ResponseEntity<User> findByUserName(@PathVariable String username) {
 		User user = userRepository.findByUsername(username);
+
+		if (user == null) {
+			log.error("User not found: " + username);
+		} else {
+			log.debug("Found user with name: " + user.getUsername());
+		}
 		return user == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(user);
 	}
 	
@@ -40,6 +56,7 @@ public class UserController {
 		user.setUsername(createUserRequest.getUsername());
 		String password = createUserRequest.getPassword();
 		if (!passwordValid(password, createUserRequest.getConfirmPassword())) {
+			log.debug("Tried to create username with invalid password");
 			return ResponseEntity.badRequest().build();
 		} else {
 			user.setPassword(bCryptPasswordEncoder.encode(password));
@@ -51,7 +68,13 @@ public class UserController {
 		user.setCart(cart);
 		userRepository.save(user);
 
-		return ResponseEntity.ok(user);
+		ResponseEntity<User> userResponse = ResponseEntity.ok(user);
+		if (userResponse.getStatusCode().isError()) {
+			log.error("Could not create user: " + user.getUsername());
+		} else {
+			log.debug("Created user with name: " + user.getUsername());
+		}
+		return userResponse;
 	}
 
 	private boolean passwordValid(String password, String confirmPassword) {
