647009a2168f8b8bbfd25299a013bf117b7565a4
Matthias Frei <matzefrei@gmail.com>
2022-01-10 17:30:28 +0100

    log exceptions for auth process

--- starter_code/src/main/java/com/example/demo/security/JwtAuthenticationVerificationFilter.java
+++ starter_code/src/main/java/com/example/demo/security/JwtAuthenticationVerificationFilter.java
@@ -1,6 +1,8 @@
 package com.example.demo.security;
 
 import com.auth0.jwt.JWT;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -19,6 +21,8 @@ import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
 
 @Component
 public class JwtAuthenticationVerificationFilter extends BasicAuthenticationFilter {
+
+    public static final Logger log = LoggerFactory.getLogger(JwtAuthenticationVerificationFilter.class);
 
     public JwtAuthenticationVerificationFilter(AuthenticationManager authenticationManager) {
         super(authenticationManager);
@@ -44,7 +48,11 @@ public class JwtAuthenticationVerificationFilter extends BasicAuthenticationFilt
         String header = req.getHeader(SecurityConstants.HEADER_STRING);
 
         if (header == null || !header.startsWith(SecurityConstants.TOKEN_PREFIX)) {
-            chain.doFilter(req, res);
+            try {
+                chain.doFilter(req, res);
+            } catch (IOException | ServletException e) {
+                log.error("Error processing authentication: " + e.getMessage());
+            }
             return;
         }
 
