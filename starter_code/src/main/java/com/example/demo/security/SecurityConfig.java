dd345561d8722729fb4c85ae608a8aa182c6baf6
Matthias Frei <matzefrei@gmail.com>
2022-01-19 16:36:40 +0100

    Fix infinite loop caused by SecurityConfig

--- starter_code/src/main/java/com/example/demo/security/SecurityConfig.java
+++ starter_code/src/main/java/com/example/demo/security/SecurityConfig.java
@@ -46,9 +46,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.parentAuthenticationManager(authenticationManagerBean())
-                .userDetailsService(userDetailsService)
-                .passwordEncoder(bCryptPasswordEncoder);
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
     }
 
 }
